dd089030a4453f53c5b0b0e998bf8a42df54e9b5
gebernoidscaletta <gebernoidscaletta@gmail.com>
2026-03-10 23:01:30 +0700

    remove creds from constantsgeneral

--- constants/constantsGeneral.py
+++ constants/constantsGeneral.py
@@ -1,6 +1,27 @@
 class ConstantsGeneral:
-    _API_USERNAME = ""
-    _API_PASSWORD = ""
+    _CARTRACK_API_USERNAME = ""
+    _CARTRACK_API_PASSWORD = ""
     _CARTRACK_INDONESIA_BASE_URL = "https://fleetapi-id.cartrack.com/rest"
     _TALENTA_DEVELOPMENT_BASE_URL = "https://sandbox-api.mekari.com"
     _TALENTA_PRODUCTION_BASE_URL = "https://api.talenta.com"
+    
+    @staticmethod
+    def getCartrackApiUsername():
+        return ConstantsGeneral._CARTRACK_API_USERNAME
+    
+    @staticmethod
+    def getCartrackApiPassword():
+        return ConstantsGeneral._CARTRACK_API_PASSWORD
+    
+    @staticmethod
+    def getCartrackIndonesiaBaseUrl():
+        return ConstantsGeneral._CARTRACK_INDONESIA_BASE_URL
+    
+    @staticmethod
+    def getTalentaDevelopmentBaseUrl():
+        return ConstantsGeneral._TALENTA_DEVELOPMENT_BASE_URL
+    
+    @staticmethod
+    def getTalentaProductionBaseUrl():
+        return ConstantsGeneral._TALENTA_PRODUCTION_BASE_URL
+    
